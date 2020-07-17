13f674142f5ad739c586976b4b6f029fe6c422f5
Bismarck-GM <geromorla@yahoo.com.ar>
2020-07-17 14:54:33 -0300

    Add posts MVC

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :timeoutable
+  devise :database_authenticatable, :timeoutable, :registerable
+
+  validates :username, :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :username, length: { minimum: 3 }
+  validates :password, length: { minimum: 6 }
+
+  has_many :posts
 end
