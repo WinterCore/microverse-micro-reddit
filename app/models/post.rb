82b2455303de88cb85587044aa25f1b4cec16b7c
WinterCore <hogobbl@gmail.com>
2020-07-21 00:22:53 +0200

    Finished comments CRUD

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, :body, presence: true, length: { minimum: 3 }
+
   belongs_to :user
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'posts#index'
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', sign_up: 'register' }
-  resources :posts
-  
+  resources :posts do
+    resources :comments, only: %i[create destroy]
+  end
 end
